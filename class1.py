6eeb72d48f2121bf499a036f4475586014e3ee69
MD Golam Rabbi <rabbi.kaunia@gmail.com>
2022-10-23 09:44:35 +0600

    first commit

---
+++ class1.py
@@ -0,0 +1,40 @@
+# from html.entities import name2codepoint
+# import numbers
+# from pickletools import long1
+# from tokenize import Double
+# from turtle import clear
+
+
+# name ='Rab'
+# age = 22
+# print ('name :',name + '    age :',age)
+# print(age)
+# name2 ="Rab"
+# age2 = 44
+# print ('name2 :',name2 + '    age2',age2 )
+# print (age2)
+
+# float
+# Double  = +8801745678952
+
+
+# x = 2
+# y = 5 
+# print ("Your Results =", x+y)
+# x=2
+# y=3
+# print(x+y)
+
+
+# x = input ("Enter your First Number")
+# y = input ("Enter your Second Nummber")
+# sum = int(x) + int (y) 
+# print ("Your Results : ", sum)
+
+
+
+from tkinter import Y
+
+
+
+
---
+++ class4.py
@@ -0,0 +1,9 @@
+# List
+student = ["Rab",918822,"Computer","hafiz",918784,"Computer","Rokon",133397,"Desktop"]
+print (student[0:9])
+print (student[0:3])
+print(student[3:6])
+print(student[6:9])
+# print (student [1])
+# print (student)
+
