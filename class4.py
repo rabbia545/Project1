4479c923b8ff1ca4bfdf604e66e34c82ff5f1126
MD Golam Rabbi <rabbi.kaunia@gmail.com>
2022-10-23 10:45:46 +0600

    list set

--- class4.py
+++ class4.py
@@ -1,29 +1,49 @@
-# List
+# # List
 
 
 
 
-student = ["Rab",918822,"Computer","hafiz",918784,"Computer","Rokon",133397,"Desktop"]
+# student = ["Rab",918822,"Computer","hafiz",918784,"Computer","Rokon",133397,"Desktop"]
 
-student [8] = "Computer"
-print (student)
-
-
-student.append ("Belal")
-print (student)
-
-student.insert (4,"hujur")
-print (student)
-student.extend (["Rokon","Desktop","dekhio","na"])
-print (student)
-
-
-
-
-# print (student[0:9])
-# print (student[0:3])
-# print(student[3:6])
-# print(student[6:9])
-# print (student [1])
+# student [8] = "Computer"
 # print (student)
+
+
+# student.append ("Belal")
+# print (student)
+
+# student.insert (4,"hujur")
+# print (student)
+# student.extend (["Rokon","Desktop","dekhio","na"])
+# print (student)
+
+
+
+
+# # print (student[0:9])
+# # print (student[0:3])
+# # print(student[3:6])
+# # print(student[6:9])
+# # print (student [1])
+# # print (student)
+
+
+
+
+# # # # # tuple fixed
+
+# student = ("Rab", 918822, "Computer", "Rokon", 13397, "Computer", "Hafiz", 918784, "Computer")
+# print (student)
+
+
+
+
+
+##### Set  v
+
+
+teacher = {"Rab", 918822, "Computer", "Rab", 918822, "Computer", "Rab", 918822, "Computer", "Rab", 918822, "Computer",}
+print (teacher)
+# teacher[2] = "tech"
+print(teacher)
 
