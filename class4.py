e71b623175af0d36dfb0029818da3885e5ae03d9
MD Golam Rabbi <rabbi.kaunia@gmail.com>
2022-10-23 10:17:41 +0600

    homework

--- class4.py
+++ class4.py
@@ -1,9 +1,29 @@
 # List
+
+
+
+
 student = ["Rab",918822,"Computer","hafiz",918784,"Computer","Rokon",133397,"Desktop"]
-print (student[0:9])
-print (student[0:3])
-print(student[3:6])
-print(student[6:9])
+
+student [8] = "Computer"
+print (student)
+
+
+student.append ("Belal")
+print (student)
+
+student.insert (4,"hujur")
+print (student)
+student.extend (["Rokon","Desktop","dekhio","na"])
+print (student)
+
+
+
+
+# print (student[0:9])
+# print (student[0:3])
+# print(student[3:6])
+# print(student[6:9])
 # print (student [1])
 # print (student)
 
