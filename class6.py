896c4ae01caa7213d93ee677c49f32c61183691f
MD Golam Rabbi <rabbi.kaunia@gmail.com>
2022-10-30 10:59:45 +0600

    fgdsg

---
+++ class6.py
@@ -0,0 +1,66 @@
+class Student:
+    def add(self,a,b):
+        return a+b
+student_obj = Student()
+print (student_obj.add (33,17))
+
+
+
+
+
+
+class Teacher:
+    def kal (self):
+        a=40
+        b=60
+        return a+b
+obje=Teacher()
+print(obje.kal())
+
+
+
+
+
+
+class Tech :
+    def hulk (self):
+        a=50
+        b=90
+        return a-b
+objec=Tech()
+print(objec.hulk())
+
+class Iron:
+    def ultra(self,a,b):
+        return a * b
+ject = Iron()
+print(ject.ultra(55,90))
+
+
+class Rab:
+    def bbi(self,a,b):
+        return a/b
+vvr = Rab()
+print(vvr.bbi(80,10))
+
+class Teach:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def add(self):
+        return self.a * self.b
+    def add1(self):
+        return self.a / self.b    
+    def add2(self):
+        return self.a + self.b
+    def add3(self):
+        return self.a - self.b
+    
+ver = Teach(50,50)
+print(ver.add())
+print(ver.add1())
+print(ver.add2())
+print(ver.add3())
+
+
+
